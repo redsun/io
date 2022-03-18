11da6e31e78e570e89306763d5bec87d2991cec5
redsun <i@didset.com>
2022-03-18 17:38:36 +0800

    ico config

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220318.1510';
+const cacheName = 'chirpy-20220318.1737';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
