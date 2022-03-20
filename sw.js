547a2f047282f28a0f8812fb99395c042e245b95
redsun <i@didset.com>
2022-03-20 20:19:45 +0800

    add draft

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220319.2127';
+const cacheName = 'chirpy-20220320.2019';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
